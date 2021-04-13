b386c84629b98f25b68b379e22575d038e0ce9bd
Touch <chantouchsek.cs83@gmail.com>
2021-04-13 21:40:43 +0700

    fix: :bug: type

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,13 @@
-import Vue from 'vue';
 import TuiCalendar from 'tui-calendar';
+import Vue, { PluginFunction } from 'vue';
 
 type FunctionKeys<T extends object> = {[K in keyof T]: T[K] extends Function ? K : never}[keyof T];
 
 type CalendarFnKeys = FunctionKeys<TuiCalendar>;
 
-export class Calendar extends Vue {
+export default class VueCalendar extends Vue {
   public invoke<T extends CalendarFnKeys>(fname: T, ...args: Parameters<TuiCalendar[T]>): ReturnType<TuiCalendar[T]>;
   public getRootElement(): HTMLElement;
+
+  static install(): PluginFunction<any>;
 }
